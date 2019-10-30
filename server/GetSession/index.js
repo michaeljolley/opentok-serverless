03d1f8a6dab4eab8f3bf214ed15a255996a2e9e1
Michael Jolley <mike@sparcapp.io>
2019-10-30 14:13:46 -0500

    Moving server logic into separate directory

---
+++ server/GetSession/index.js
@@ -0,0 +1,64 @@
+const faunaDB = require("../_fauna");
+const openTok = require("../_opentok");
+
+/**
+ * Gets or creates a session based on the provided parameters
+ * @param sessionName Unique name of the session to join
+ * @param mediaMode Media settings the session should use. Defaults to 'routed'
+ * @param archiveMode Archive settings for the session. Defaults to 'manual'
+ * @param location Optional location hint
+ */
+module.exports = async function (context, req) {
+  context.log("GetSession trigger processed a request.");
+
+  if (req.query.sessionName || (req.body && req.body.sessionName)) {
+    let sessionName = req.query.sessionName || req.body.sessionName;
+    sessionName = sessionName.toLowerCase();
+
+    let session = await faunaDB.getSession(sessionName);
+
+    if (session) {
+      console.log(`session: ${session.sessionId}`);
+      context.res = {
+        body: session
+      };
+    } else {
+      const archiveMode =
+        req.query.archiveMode || (req.body && req.body.archiveMode) || "manual";
+      const location =
+        req.query.location || (req.body && req.body.location) || undefined;
+      const mediaMode =
+        req.query.mediaMode || (req.body && req.body.mediaMode) || "routed";
+      const p2pPreference =
+        req.query["p2p.preference"] ||
+        (req.body && req.body.p2p && req.body.p2p.preference) ||
+        "disabled";
+
+      const sessionId = await openTok.createSession(
+        archiveMode,
+        location,
+        mediaMode,
+        p2pPreference
+      );
+
+      if (sessionId) {
+        console.log(`session: ${sessionId}`);
+        session = await faunaDB.createSession(sessionId, sessionName);
+        context.res = {
+          body: session
+        };
+      } else {
+        context.res = {
+          status: 400,
+          body: "Unable to create or retrieve session."
+        };
+      }
+    }
+  } else {
+    context.res = {
+      status: 400,
+      body:
+        "Please pass a sessionName on the query string or in the request body"
+    };
+  }
+};
---
+++ server/SessionMonitorCallback/index.js
@@ -0,0 +1,39 @@
+const faunaDB = require("../_fauna");
+const openTok = require("../_opentok");
+
+module.exports = async function (context, req) {
+  context.log("SessionMonitorCallback trigger function processed a request.");
+
+  if (req.body) {
+    const payload = req.body;
+
+    let sessionRecord = await faunaDB.getRecord(payload.sessionId);
+
+    if (sessionRecord) {
+
+      let members = sessionRecord.data.members;
+
+      switch (payload.event) {
+        case "connectionCreated":
+          members++;
+          break;
+
+        case "connectionDestroyed":
+          members--;
+          break;
+      }
+
+      if (members < 0) {
+        members = 0;
+      }
+
+      sessionRecord.data.members = members;
+
+      await faunaDB.updateSession(sessionRecord);
+    }
+  }
+
+  context.res = {
+    body: "Ok"
+  };
+};
